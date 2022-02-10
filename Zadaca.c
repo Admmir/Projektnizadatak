5d48576e720059fc144a1a1d803ac9bcfef20689
Admmir <81434553+Admmir@users.noreply.github.com>
2022-02-10 21:25:50 +0100

    Add files via upload

---
+++ Zadaca.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "Funkcije.h"
+
+
+
+
+
+int main(){
+
+init_load(studenti);
+print_studenti();
+meni();
+
+
+
+
+
+
+return 0;
+}
